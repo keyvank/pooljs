69a5700a71696269f7b639ae462a869645139206
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-03-11 18:05:27 +0330

    much better structure

--- server.py
+++ server.py
@@ -10,30 +10,27 @@ WORKERS_SERVER_PORT = 5678
 COMMANDERS_SERVER_IP = '127.0.0.1'
 COMMANDERS_SERVER_PORT = 5679
 
-counter = 0
-
 jobs = asyncio.Queue()
 
-id_websocket = []
-websocket_outgoing = []
-id_assigner = 0
+task_counter = 0
+task_queues = []
 
 async def commanders_handler(websocket, path):
+	global task_counter
+	
+	websocket_queue = asyncio.Queue()
+
 	try:
-		global counter
-		global id_assigner
-		if not hasattr(websocket,'id'):
-			websocket.id = id_assigner
-			websocket_outgoing.append(asyncio.Queue())
-			id_assigner += 1
+		
 		incoming = None
 		job_result = None
 		while True:
-			print((len(id_websocket),len(websocket_outgoing)))
+			#print((len(id_websocket),len(websocket_queues)))
+			
 			if not incoming or incoming.done():
 				incoming = asyncio.ensure_future(websocket.recv()) # Wait for an incoming message
 			if not job_result or job_result.done():
-				job_result = asyncio.ensure_future(websocket_outgoing[websocket.id].get())
+				job_result = asyncio.ensure_future(websocket_queue.get())
 			
 
 			done, pending = await asyncio.wait( # Wait for a incoming message or a job result to send
@@ -42,9 +39,9 @@ async def commanders_handler(websocket, path):
 
 			if incoming in done: # If there is a incoming message
 				obj = json.loads(incoming.result())
-				id_websocket.append(websocket)
-				await jobs.put({"id": counter, "code": obj["code"]})
-				counter += 1
+				task_queues.append(websocket_queue)
+				await jobs.put({"id": task_counter, "code": obj["code"]})
+				task_counter += 1
 
 			if job_result in done: # If there is a job result to send
 				result = job_result.result()
@@ -57,7 +54,7 @@ async def workers_handler(websocket, path):
 		incoming = None
 		new_job = None
 		while True:
-			print((len(id_websocket),len(websocket_outgoing)))
+			#print((len(id_websocket),len(websocket_queues)))
 			if not incoming or incoming.done():
 				incoming = asyncio.ensure_future(websocket.recv()) # Wait for an incoming message
 			if not new_job or new_job.done():
@@ -69,13 +66,13 @@ async def workers_handler(websocket, path):
 
 			if incoming in done: # If there is a incoming message
 				msg = json.loads(incoming.result())
-				await websocket_outgoing[id_websocket[msg["id"]].id].put(msg)
+				await task_queues[msg["id"]].put(msg)
 
 			if new_job in done: # If there is a new job to send
 				job = new_job.result()
 				await websocket.send(json.dumps(job))
 	except websockets.ConnectionClosed:
-		print("Closed!")
+		print("Closed")
 
 workers_server = websockets.serve(workers_handler, WORKERS_SERVER_IP, WORKERS_SERVER_PORT)
 commanders_server = websockets.serve(commanders_handler, COMMANDERS_SERVER_IP, COMMANDERS_SERVER_PORT)
