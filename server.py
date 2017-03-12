e85b4750bc74231ff0f9863fa945d9bfc97d09f6
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-03-12 17:42:00 +0330

    uniform random distribution of tasks between sockets

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 import asyncio
 import websockets
 import json
+import random
 
 WORKERS_SERVER_IP = '127.0.0.1'
 WORKERS_SERVER_PORT = 5678
@@ -14,6 +15,9 @@ jobs = asyncio.Queue()
 
 task_counter = 0
 task_queues = {}
+
+worker_websockets = set()
+
 
 async def commanders_handler(websocket, path):
 	global task_counter
@@ -50,30 +54,26 @@ async def commanders_handler(websocket, path):
 		print("Closed!")
 
 async def workers_handler(websocket, path):
+	worker_websockets.add(websocket)
 	try:
 		incoming = None
 		new_job = None
 		while True:
 			print(len(task_queues))
-			if not incoming or incoming.done():
-				incoming = asyncio.ensure_future(websocket.recv()) # Wait for an incoming message
-			if not new_job or new_job.done():
-				new_job = asyncio.ensure_future(jobs.get()) # Wait for a new job to send
 
-			done, pending = await asyncio.wait( # Wait for a incoming message or new job to send
-				[incoming, new_job],
-				return_when=asyncio.FIRST_COMPLETED)
-
-			if incoming in done: # If there is a incoming message
-				msg = json.loads(incoming.result())
-				await task_queues[msg["id"]].put(msg)
-				del task_queues[msg["id"]]
-
-			if new_job in done: # If there is a new job to send
-				job = new_job.result()
-				await websocket.send(json.dumps(job))
+			msg = json.loads(await websocket.recv())
+			await task_queues[msg["id"]].put(msg)
+			del task_queues[msg["id"]]
 	except websockets.ConnectionClosed:
+		worker_websockets.remove(websocket)
 		print("Closed")
+
+async def balance_handler():
+	while True:
+		job = await jobs.get()
+		websocket = random.sample(worker_websockets, 1)[0]
+		await websocket.send(json.dumps(job))
+	
 
 workers_server = websockets.serve(workers_handler, WORKERS_SERVER_IP, WORKERS_SERVER_PORT)
 commanders_server = websockets.serve(commanders_handler, COMMANDERS_SERVER_IP, COMMANDERS_SERVER_PORT)
@@ -81,7 +81,7 @@ commanders_server = websockets.serve(commanders_handler, COMMANDERS_SERVER_IP, C
 loop = asyncio.get_event_loop()
 
 try:
-	loop.run_until_complete(asyncio.gather(workers_server,commanders_server))
+	loop.run_until_complete(asyncio.gather(workers_server,commanders_server,balance_handler()))
 	loop.run_forever()
 except KeyboardInterrupt:
 	print("Server stopped!")
