3fffc73b19ff7f1bb31b9a30c7da914daaf7cda9
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-03-14 01:03:40 +0330

    created django project and project structure

---
+++ django_project/main/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from .views import *
+
+urlpatterns = [
+	url(r'^$',index,name="main_index"),
+	url(r'^worker/$',worker,name="main_worker"),
+	url(r'^commander/$',commander,name="main_commander"),
+]
---
+++ django_project/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+	return render(request,'main/index.html',{})
+
+def worker(request):
+	return render(request,'main/worker.html',{})
+
+def commander(request):
+	return render(request,'main/commander.html',{})
