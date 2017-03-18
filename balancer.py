7c86d3655f3839099a3f3992bec8f61e37148564
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-03-19 01:22:32 +0330

    some exceptions resolved

--- balancer.py
+++ balancer.py
@@ -49,12 +49,13 @@ class WorkerProtocol(WebSocketServerProtocol):
 	async def cleanup(self):
 		if hasattr(self,"jobs"):
 			for j in self.jobs:
-				jobs[j][3]+=1
-				if jobs[j][3] > MAX_FAILURES:
-					jobs[j][1].result_available(j,None,True)
-					del jobs[j]
-				else:
-					await job_queue.put(j)
+				if j in jobs: # Notice this
+					jobs[j][3]+=1
+					if jobs[j][3] > MAX_FAILURES:
+						jobs[j][1].result_available(j,None,True)
+						del jobs[j]
+					else:
+						await job_queue.put(j)
 			del self.jobs[:]
 		print_info()
 	
