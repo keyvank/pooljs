eeabb3227398e2736f6a42765e8b7e6a1a3cf239
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-03-19 00:49:53 +0330

    worker not blocking

--- django_project/static/js/worker.js
+++ django_project/static/js/worker.js
@@ -1,9 +1,5 @@
 (function (context){
 
-	var WEBSOCKET_HOST = "pooljs.ir";
-	var WEBSOCKET_PORT = 12121;
-	var WEBSOCKET_ADDRESS = "wss://" + WEBSOCKET_HOST + ":" + WEBSOCKET_PORT;
-
 	if(window && "WebSocket" in window && typeof(Worker) !== "undefined") {
 		function createWorker(foo){
 			var str = foo.toString().match(/^\s*function\s*\(\s*\)\s*\{(([\s\S](?!\}$))*[\s\S])/)[1];
@@ -13,43 +9,32 @@
 		var workerPool = [];
 		for(var i = 0; i < navigator.hardwareConcurrency; i++){
 			var worker = createWorker(function(){
+				var WEBSOCKET_HOST = "pooljs.ir";
+				var WEBSOCKET_PORT = 12121;
+				var WEBSOCKET_ADDRESS = "wss://" + WEBSOCKET_HOST + ":" + WEBSOCKET_PORT;
 				var self = this;
-				this.addEventListener("message", function(event) {
-					var job = event.data;
-					var src = "var fn = " + job.code;
-					eval(src);
-					var job_result = {"id":job.id,"result":fn.apply(this,job.args)};
-					self.postMessage(job_result);
-				}, false);
+				function startSocket(){
+					var sock = new WebSocket(WEBSOCKET_ADDRESS);
+
+					sock.onopen = function(){
+					};
+					sock.onmessage = function(event){
+						var job = JSON.parse(event.data);
+						var src = "var fn = " + job.code;
+						eval(src);
+						var job_result = {"id":job.id,"result":fn.apply(this,job.args)};
+						sock.send(JSON.stringify(job_result));
+					};
+					sock.onclose = function(){
+						setTimeout(startSocket,5000);
+					};
+				}
+				startSocket();
 			});
 			workerPool.push(worker);
 		}
 		context.worker = {}
 
-		function startSocket(){
-			var sock = new WebSocket(WEBSOCKET_ADDRESS);
-
-			function response(event){
-				var job_result = event.data;
-				sock.send(JSON.stringify(job_result));
-			}
-
-			function balance(job){
-				var w = workerPool[Math.floor(Math.random()*workerPool.length)];
-				w.onmessage = response;
-				w.postMessage(job);
-			}
-
-			sock.onopen = function(){
-			};
-			sock.onmessage = function(event){
-				var job = JSON.parse(event.data);
-				balance(job);
-			};
-			sock.onclose = function(){
-				setTimeout(startSocket,5000);
-			};
-		}
-		startSocket();
+		
 	}
 }(this));
