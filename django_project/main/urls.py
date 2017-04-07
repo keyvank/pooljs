358062fc757a4f678028681c15a9529b16f9d4b0
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-04-08 01:57:12 +0430

    url structures created

--- django_project/main/urls.py
+++ django_project/main/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
 	url(r'^$',index,name="main_index"),
-	url(r'^worker/$',worker,name="main_worker"),
-	url(r'^raytracer/$',raytracer,name="main_raytracer"),
+	url(r'^docs/$',docs,name="main_docs"),
 	url(r'^sandbox/$',sandbox,name="main_sandbox"),
 ]
--- django_project/main/views.py
+++ django_project/main/views.py
@@ -3,11 +3,8 @@ from django.shortcuts import render
 def index(request):
 	return render(request,'main/index.html',{})
 
-def worker(request):
-	return render(request,'main/worker.html',{})
-
 def sandbox(request):
 	return render(request,'main/sandbox.html',{})
 
-def raytracer(request):
-	return render(request,'main/raytracer.html',{})
+def docs(request):
+	return render(request,'main/docs.html',{})
