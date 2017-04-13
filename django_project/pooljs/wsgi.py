75b88cdff4fc46c5eec0275c5fbabef823e98711
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-04-13 18:10:20 +0430

    replace codename raisin to pooljs

--- django_project/pooljs/wsgi.py
+++ django_project/pooljs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for raisin project.
+WSGI config for pooljs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "raisin.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pooljs.settings")
 
 application = get_wsgi_application()
