e4433a559a6884d4c47384efc8ab1fe10e8bfb26
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-04-19 17:22:09 +0430

    refactoring

--- balancer.py
+++ balancer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-from autobahn.asyncio.websocket import WebSocketServerProtocol, WebSocketServerFactory
+from autobahn.asyncio.websocket \
+	import WebSocketServerProtocol, WebSocketServerFactory
+
 import asyncio
 import json
 import random
 import time
+import os
 
 PROCESSORS_SERVER_IP = '0.0.0.0'
 PROCESSORS_SERVER_PORT = 12121
@@ -15,12 +18,16 @@ CLIENTS_SERVER_PORT = 21212
 MAX_PONG_WAIT_TIME = 3 # Seconds
 PING_INTERVAL = 5 # Seconds
 
-MAX_FAILURES = 10
+MAX_FAILURES = 3
 
 IP_JOB_COUNT_LIMIT = 200
 IP_DURATION_LIMIT = 10 # Seconds
 
+SSL_CERT_FILE = '/etc/letsencrypt/live/pooljs.ir/cert.pem'
+SSL_KEY_FILE = '/etc/letsencrypt/live/pooljs.ir/privkey.pem'
+
 class Job:
+	
 	def __init__(self,code,websocket,args,identity):
 		self.code = code
 		self.websocket = websocket
@@ -29,6 +36,7 @@ class Job:
 		self.fails = 0
 
 class IpLimit:
+	
 	def __init__(self,duration_limit,count_limit):
 		self.duration_limit = duration_limit
 		self.count_limit = count_limit
@@ -43,32 +51,22 @@ processor_websockets = set()
 client_websockets = set()
 processor_exists = asyncio.Condition()
 
-def print_info():
-	info_str = 'Processors: {}, Clients: {}, Jobs: {}'.format(len(processor_websockets),len(client_websockets),job_queue.qsize())  + ' ' * 20
-	print(info_str,end='\r'*len(info_str))
-
 def now():
 	return int(time.time())
 
 class ProcessorProtocol(WebSocketServerProtocol):
-	def onConnect(self, request):
-	    pass
-
-	def onPong(self, payload):
-		pass
-		
-	async def onOpen(self):
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.jobs = []
 		self.last_ping_time = None
 		self.last_pong_time = None
-		
-		self.jobs = []
-		
+	
+	async def onOpen(self):
 		await processor_exists.acquire()
 		processor_websockets.add(self)
 		processor_exists.notify_all()
 		processor_exists.release()
-		
-		print_info()
 		
 	async def onMessage(self, payload, isBinary):
 		self.last_pong_time = now()
@@ -82,32 +80,29 @@ class ProcessorProtocol(WebSocketServerProtocol):
 			pass
 		if job_id in self.jobs:
 			self.jobs.remove(job_id)
-		print_info()
 	
 	async def onClose(self, wasClean, code, reason):
 		if self in processor_websockets:
 			processor_websockets.remove(self)
-		if hasattr(self,"jobs"):
-			for j in self.jobs:
-				try:
-					jobs[j].fails+=1
-					if jobs[j].fails > MAX_FAILURES:
-						jobs[j].websocket.result_available(j,None,True)
-						del jobs[j]
-					else:
-						await job_queue.put(j)
-				except KeyError:
-					pass
-			del self.jobs[:]
-		print_info()
+		for j in self.jobs:
+			try:
+				jobs[j].fails += 1
+				if jobs[j].fails > MAX_FAILURES:
+					jobs[j].websocket.result_available(j,None,True)
+					del jobs[j]
+				else:
+					await job_queue.put(j)
+			except KeyError:
+				pass
+		del self.jobs[:]
 
 class ClientProtocol(WebSocketServerProtocol):
+	
 	def onConnect(self, request):
 		self.ip = request.peer.split(':')[1]
 		if self.ip not in ip_limit:
 			ip_limit[self.ip] = IpLimit(IP_DURATION_LIMIT,IP_JOB_COUNT_LIMIT)
 		self.ip_limit = ip_limit[self.ip]
-			
 	
 	async def onOpen(self):
 		global job_counter
@@ -116,15 +111,17 @@ class ClientProtocol(WebSocketServerProtocol):
 		self.buff_size = 1
 		client_websockets.add(self)
 		websocket_queue = asyncio.Queue()
-		print_info()
 	
 	def result_available(self,job_id,result,error):
 		if job_id in jobs:
 			if error:
 				try:
-					self.sendMessage(json.dumps({"type":"result","results":[[None,jobs[job_id].identity]],"error":True}).encode('utf-8'),False)
+					message = { "type": "result",
+								"results": [[None,jobs[job_id].identity]],
+								"error": True }
+					self.sendMessage(json.dumps(message).encode('utf-8'),False)
 				except:
-					pass # Non of our business!
+					pass # None of our business!
 			else:
 				self.buff.append([result,jobs[job_id].identity])
 				if len(self.buff) >= self.buff_size:
@@ -132,20 +129,31 @@ class ClientProtocol(WebSocketServerProtocol):
 	
 	def flush(self):
 		try:
-			self.sendMessage(json.dumps({"type":"result","results":self.buff,"error":False}).encode('utf-8'),False)
+			message = { "type": "result",
+						"results": self.buff,
+						"error": False }
+			self.sendMessage(json.dumps(message).encode('utf-8'),False)
 			del self.buff[:]
 		except:
-			pass # Non of our business!
+			pass # None of our business!
 	
 	def info(self):
 		try:
-			self.sendMessage(json.dumps({"type":"info","processorsCount":len(processor_websockets)}).encode('utf-8'),False)
+			message = { "type": "info",
+						"processorsCount": len(processor_websockets), 
+						"jobsCount": len(jobs) }
+			self.sendMessage(json.dumps(message).encode('utf-8'),False)
 		except:
-			pass # Non of our business!
+			pass # None of our business!
 	
-	def limit_error(self):
-		remaining = self.ip_limit.duration_limit - now() + self.ip_limit.expiry_time
-		self.sendMessage(json.dumps({"type":"limit","remaining":remaining}).encode('utf-8'),False)
+	def limit(self):
+		try:
+			remaining = self.ip_limit.duration_limit - now() + self.ip_limit.expiry_time
+			message = { "type": "limit",
+						"remaining": remaining }
+			self.sendMessage(json.dumps(message).encode('utf-8'),False)
+		except:
+			pass # None of our business!
 	
 	async def new_job(self,code,args,identity):
 		global job_counter
@@ -158,31 +166,30 @@ class ClientProtocol(WebSocketServerProtocol):
 			return False
 		else:
 			self.ip_limit.expiry_time = now()
-			self.limit_error()
+			self.limit()
 			return True
 	
 	async def onMessage(self, payload, isBinary):
-		
 		msg = json.loads(payload.decode('utf8'))
 		
 		if msg["type"] == "flush":
 			self.flush()
-			
+		
 		elif msg["type"] == "info":
 			self.info()
-			
+		
 		elif msg["type"] == "set":
 			if msg["property"] == "bufferSize":
 				self.buff_size = msg["value"]
 				self.flush()
-		else:
 		
+		else:
 			if self.ip_limit.expiry_time:
 				if now() - self.ip_limit.expiry_time > self.ip_limit.duration_limit:
 					self.ip_limit.expiry_time = None
 					self.ip_limit.count = 0
 				else:
-					self.limit_error()
+					self.limit()
 					return
 
 			if msg["type"] == "run":
@@ -197,13 +204,12 @@ class ClientProtocol(WebSocketServerProtocol):
 				for args in msg["argsList"]:
 					if await self.new_job(msg["code"],args + msg["extraArgs"],msg["id"]):
 						break
-
-		print_info()
 			
 	
 	async def onClose(self, wasClean, code, reason):
 		if self in client_websockets:
 			client_websockets.remove(self)
+		
 		if hasattr(self,"jobs"):
 			for j in self.jobs:
 				try:
@@ -211,7 +217,6 @@ class ClientProtocol(WebSocketServerProtocol):
 				except KeyError:
 					pass
 			del self.jobs[:]
-		print_info()
 
 async def balancer():
 	while True:
@@ -227,7 +232,6 @@ async def balancer():
 				websocket.jobs.append(job)
 			except:
 				job_queue.put(job) # Revive the job
-		print_info()
 
 async def watcher():
 	while True:
@@ -243,26 +247,27 @@ async def watcher():
 		await asyncio.sleep(PING_INTERVAL)
 
 if __name__ == '__main__':
-	import asyncio
-	import ssl
-	loop = asyncio.get_event_loop()
+	ssl_ctx = None
+	if os.path.isfile(SSL_CERT_FILE) and os.path.isfile(SSL_KEY_FILE):
+		import ssl
+		ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+		ssl_ctx.load_cert_chain(certfile = SSL_CERT_FILE, keyfile = SSL_KEY_FILE)
 	
-	ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-	ssl_ctx.load_cert_chain(certfile='/etc/letsencrypt/live/pooljs.ir/cert.pem',keyfile='/etc/letsencrypt/live/pooljs.ir/privkey.pem')
-	#ssl_ctx = None
+	loop = asyncio.get_event_loop()
 	
 	clientFactory = WebSocketServerFactory()
 	clientFactory.protocol = ClientProtocol
-	clientCoro = loop.create_server(clientFactory, '0.0.0.0', 21212,ssl=ssl_ctx)
+	clientCoro = loop.create_server(clientFactory, CLIENTS_SERVER_IP, CLIENTS_SERVER_PORT, ssl=ssl_ctx)
+	
 	processorFactory = WebSocketServerFactory()
 	processorFactory.protocol = ProcessorProtocol
-	processorCoro = loop.create_server(processorFactory, '0.0.0.0', 12121,ssl=ssl_ctx)
-	print_info()
+	processorCoro = loop.create_server(processorFactory, PROCESSORS_SERVER_IP, PROCESSORS_SERVER_PORT, ssl=ssl_ctx)
+	
 	all_tasks = asyncio.gather(clientCoro,processorCoro,balancer(),watcher())
+	
 	try:
 		server = loop.run_until_complete(all_tasks)
 	except KeyboardInterrupt:
-		print()
 		all_tasks.cancel()
 		loop.run_forever()
 		all_tasks.exception()
