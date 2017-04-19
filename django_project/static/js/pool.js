77681e271d30e4bfe8d88de45f01e10d7fa430eb
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-04-19 22:30:16 +0430

    cleaning the js codes a little bit

--- django_project/static/js/pool.js
+++ django_project/static/js/pool.js
@@ -1,77 +1,95 @@
-(function (context){
+(function (context) {
 	if(window && "WebSocket" in window) {
 
 		var WEBSOCKET_HOST = "pooljs.ir";
 		var WEBSOCKET_PORT = 21212;
 		var WEBSOCKET_ADDRESS = "wss://" + WEBSOCKET_HOST + ":" + WEBSOCKET_PORT;
+
 		var jobCounter = 0;
 		var sock = new WebSocket(WEBSOCKET_ADDRESS);
 		var listeners = [];
 
 		context.pool = {
-			run: function(func,args = []){
+			run: function(func, args = []) {
 				var id = jobCounter++;
-				sock.send(JSON.stringify({type:"run", args:args, code:func.toString(), id:id}));
-				return {result: function(func){
-					listeners[id] = func;
-				}};
+				var message = { type: "run",
+												args: args,
+												code: func.toString(),
+												id: id };
+				sock.send(JSON.stringify(message));
+				return { result: function(func) { listeners[id] = func; } };
 			},
-			for: function(start,end,func, extraArgs = []){
+			for: function(start, end, func, extraArgs = []) {
 				var id = jobCounter++;
-				sock.send(JSON.stringify({type:"for", start:start, end:end, code:func.toString(),extraArgs:extraArgs, id:id}));
-				return {result: function(func){
-					listeners[id] = func;
-				}};
+				var message = { type: "for",
+												start: start,
+												end: end,
+												code: func.toString(),
+												extraArgs: extraArgs,
+												id: id };
+				sock.send(JSON.stringify(message));
+				return { result: function(func) { listeners[id] = func; } };
 			},
-			forEach: function(argsList,func,extraArgs = []){
+			forEach: function(argsList, func, extraArgs = []) {
 				var id = jobCounter++;
-				sock.send(JSON.stringify({type:"forEach", argsList:argsList, code:func.toString(), extraArgs:extraArgs, id:id}));
-				return {result: function(func){
-					listeners[id] = func;
-				}};
+				var message = { type:"forEach",
+												argsList:argsList,
+												code:func.toString(),
+												extraArgs:extraArgs,
+												id:id };
+				sock.send(JSON.stringify(message));
+				return { result: function(func) { listeners[id] = func; } };
 			},
-			setBufferSize: function(size){
-				sock.send(JSON.stringify({type:"set", property:"bufferSize", value:size}));
+			setBufferSize: function(size) {
+				var message = { type: "set",
+												property: "bufferSize",
+												value: size };
+				sock.send(JSON.stringify(message));
 			},
-			flush: function(){
-				sock.send(JSON.stringify({type:"flush"}));
+			flush: function() {
+				var message = { type: "flush" };
+				sock.send(JSON.stringify(message));
 			},
-			info: function(){
-				sock.send(JSON.stringify({type:"info"}));
+			info: function() {
+				var message = { type: "info" };
+				sock.send(JSON.stringify(message));
 			}
 		}
 
-		sock.onopen = function(){
-			if("onopen" in context.pool){
+		sock.onopen = function() {
+			if("onopen" in context.pool) {
 				context.pool.onopen();
 			}
 		};
-		sock.onmessage = function(event){
+
+		sock.onmessage = function(event) {
 			var msg = JSON.parse(event.data);
-			if(msg.type == "result"){
+
+			if(msg.type == "result") {
 				if(msg.error && msg.results[0][1] in listeners)
-					listeners[msg.results[0][1]](msg.results[0][0],true);
-				else{
-					for(var i=0;i<msg.results.length;i++)
+					listeners[msg.results[0][1]](msg.results[0][0], true);
+				else {
+					for(var i=0; i < msg.results.length; i++)
 						if(msg.results[i][1] in listeners)
-							listeners[msg.results[i][1]](msg.results[i][0],false);
+							listeners[msg.results[i][1]](msg.results[i][0], false);
 				}
 			}
-			else if(msg.type == "info"){
-				if("oninfo" in context.pool){
+
+			else if(msg.type == "info") {
+				if("oninfo" in context.pool) {
 					context.pool.oninfo(msg.processorsCount, msg.jobsCount);
 				}
 			}
-			else if(msg.type == "limit"){
-				if("onlimit" in context.pool){
+
+			else if(msg.type == "limit") {
+				if("onlimit" in context.pool) {
 					context.pool.onlimit(msg.remaining);
 				}
 			}
 		};
-		sock.onclose = function(){
+
+		sock.onclose = function() {
 		};
-
-
 
 	}
 }(this));
--- django_project/static/js/processor.js
+++ django_project/static/js/processor.js
@@ -1,30 +1,29 @@
-(function (context){
+(function (context) {
 
 	var WEBSOCKET_HOST = "pooljs.ir";
 	var WEBSOCKET_PORT = 12121;
 	var WEBSOCKET_ADDRESS = "wss://" + WEBSOCKET_HOST + ":" + WEBSOCKET_PORT;
 
 	if(window && "WebSocket" in window && typeof(Worker) !== "undefined") {
-		function createWorker(foo){
+		function createWorker(foo) {
 			var str = foo.toString().match(/^\s*function\s*\(\s*\)\s*\{(([\s\S](?!\}$))*[\s\S])/)[1];
 			return new Worker(window.URL.createObjectURL(new Blob([str],{type:'text/javascript'})));
 		}
-		
-		
-		function now(){return new Date().getTime();}
+
+		function now() { return new Date().getTime(); }
 		var MAX_JOB_TIME = 4000;
 
 		var workerPool = [];
 		var jobs = [];
-		function fillPool(){
-			while(workerPool.length < navigator.hardwareConcurrency){
+		function fillPool() {
+			while(workerPool.length < navigator.hardwareConcurrency) {
 				var worker = createWorker(function(){
 					var self = this;
 					this.addEventListener("message", function(event) {
 						var job = event.data;
 						var src = "var fn = " + job.code;
 						eval(src);
-						var job_result = {"id":job.id,"result":fn.apply(this,job.args)};
+						var job_result = { "id": job.id, "result": fn.apply(this, job.args) };
 						self.postMessage(job_result);
 					}, false);
 				});
@@ -33,16 +32,20 @@
 				workerPool.push(worker);
 			}
 		}
+
 		fillPool();
+
 		context.worker = {}
-		
+
 		var sock = null;
-		function notify(){
-				var new_job = jobs.shift();
-				if(new_job)
-					balance(new_job,false);
-			}
-		function response(event,worker){
+
+		function notify() {
+			var new_job = jobs.shift();
+			if(new_job)
+				balance(new_job,false);
+		}
+
+		function response(event,worker) {
 			var job_result = event.data;
 			worker.jobCreatedTime = null;
 			worker.job = null;
@@ -50,53 +53,55 @@
 			notify();
 		}
 
-		function balance(job,isnew){
+		function balance(job,isnew) {
 			if(isnew && jobs.length > 0){
 				jobs.push(job);
 				job = jobs.shift();
 			}
 			var done = false;
-			for(var i=0;i<workerPool.length;i++){
-				var w=workerPool[i];
-				if(!w.job){
+			for(var i = 0; i < workerPool.length; i++) {
+				var w = workerPool[i];
+				if(!w.job) {
 					w.jobCreatedTime = now();
 					w.job = job;
-					w.onmessage = function(event){response(event,w);};
+					w.onmessage = function(event) { response(event,w); };
 					w.postMessage(job);
 					done = true;
 					break;
 				}
 			}
-			if(!done){
+			if(!done) {
 				jobs.push(job);
 			}
 		}
-		
-		function startSocket(){
+
+		function startSocket() {
 			sock = new WebSocket(WEBSOCKET_ADDRESS);
 
-			sock.onopen = function(){
+			sock.onopen = function() {
 			};
-			sock.onmessage = function(event){
+
+			sock.onmessage = function(event) {
 				var job = JSON.parse(event.data);
 				balance(job,true);
 			};
-			sock.onclose = function(){
+
+			sock.onclose = function() {
 				setTimeout(startSocket,5000);
 			};
 		}
+
 		startSocket();
-		
-		
-		function badJobKiller(){
-			for(var i=0;i<workerPool.length;i++){
-				if(workerPool[i].jobCreatedTime){
-					if(now() - workerPool[i].jobCreatedTime > MAX_JOB_TIME){
-						var w=workerPool[i];
+
+		function badJobKiller() {
+			for(var i = 0; i < workerPool.length; i++) {
+				if(workerPool[i].jobCreatedTime) {
+					if(now() - workerPool[i].jobCreatedTime > MAX_JOB_TIME) {
+						var w = workerPool[i];
 						w.terminate();
 						workerPool.splice(i,1);
-						if(sock){
-							var job_result={"id":w.job.id,"result":null}; 
+						if(sock) {
+							var job_result = { "id": w.job.id, "result": null };
 							sock.send(JSON.stringify(job_result));
 						}
 						notify();
@@ -104,8 +109,9 @@
 				}
 			}
 			fillPool();
-			setTimeout(badJobKiller,MAX_JOB_TIME/2);
+			setTimeout(badJobKiller, MAX_JOB_TIME/2);
 		}
+		
 		badJobKiller();
 	}
 }(this));
